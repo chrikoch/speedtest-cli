a3d6748304ba0692cd60389282f7b1eac8f68216
Christian Koch <chri.koch@gmail.com>
2018-12-20 20:22:56 +0100

    path to speedtest-cli as parameter

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,26 @@
 #/bin/bash
 
-line=$(/home/pi/speedtest-cli-sivel --json | jq -r '[.timestamp, .download, .upload] | @csv' | head -n 1 | sed -es/\"//g )
+speedtest_cli_dir=""
+
+while getopts "s:" optname
+do
+  case "$optname" in
+    "s")
+      speedtest_cli_dir=$OPTARG
+      ;;
+  esac
+done
+
+
+if [ -z "${speedtest_cli_dir}" ]
+then
+  echo "Give path to https://github.com/sivel/speedtest-cli with param -s"
+  exit 1
+fi
+
+
+
+line=$(${speedtest_cli_dir}/speedtest.py --json | jq -r '[.timestamp, .download, .upload] | @csv' | head -n 1 | sed -es/\"//g )
 
 
 utcdate=$(echo $line | awk 'BEGIN{FS=","}{print $1}')
